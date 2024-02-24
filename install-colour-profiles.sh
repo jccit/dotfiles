f2b9b01b7ad6b8ff831353669d3b9c40eb0c1dce
Joe Citrine <josef@citrine.dev>
2024-02-24 21:47:36 +0000

    Colour profiles

---
+++ install-colour-profiles.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Installs or updates ICC colour profiles
+
+OS=$OSTYPE
+dir=`pwd`
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  echo 'This script only works on macOS'
+  exit 1
+fi
+
+echo '-----------------------------'
+echo 'Copying ICC profiles to ~/Library/ColorSync/Profiles'
+
+cp -r ${dir}/colour-profiles/* ~/Library/ColorSync/Profiles
+
+echo 'ICC profiles installed'
+echo '-----------------------------'
